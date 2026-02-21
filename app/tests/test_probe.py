30c407d66b228c1f25a9476384327a1fd57aed82
NickRosso <nicholasrosso@comcast.net>
2026-02-20 22:14:06 -0600

    added missing protocol error test case along with a way to test # of requests using Mock on get requests and sleep.

--- app/tests/test_probe.py
+++ app/tests/test_probe.py
@@ -1,6 +1,6 @@
 from app.main import app
 from fastapi.testclient import TestClient
-import time
+from unittest.mock import patch, MagicMock
 
 client = TestClient(app)
 
@@ -52,9 +52,53 @@ def test_index():
     assert isinstance(system["memory"], dict)
     assert isinstance(system["disk_usage"], dict)
 
-def test_web_probe_required_query_parameters():
+def test_probe_url_required_query_parameters():
     response = client.get("/probe/url")
     data = response.json()
     assert response.status_code == 422
     assert isinstance(data["detail"], list) # if request does not contain required parameters 
     assert len(data["detail"])== 2 #currently there are two required query parameters
+
+@patch("app.main.time.sleep")
+@patch("app.main.requests.get")
+def test_probe_url_request_count(mock_get, mock_sleep):
+    #creating a mock object to mimick get request and time.sleep calls. 
+    # In the test we count the number of times they were called to check the underlying logic
+    mock_response = MagicMock()
+    mock_response.ok = False
+    mock_response.status_code = 500
+    mock_response.content = b"error"
+    mock_get.return_value = mock_response
+
+    response = client.get("/probe/url", params={
+        "count": 2,
+        "url": "http://localhost:8000",
+        "delay": 2, 
+        "back_off": 3,
+        "ssl": True
+    })
+
+    assert response.status_code == 200
+    assert mock_sleep.call_count == 2
+    
+    response = client.get("/probe/url", params={
+        "count": 10,
+        "url": "http://localhost:8000",
+        "delay": 2, 
+        "back_off": 3,
+    })
+
+    assert response.status_code == 200
+    #call_count should be total of count between the 2 requests
+    assert mock_sleep.call_count == 12
+
+
+def test_probe_url_missing_protocol_error():
+    response = client.get("/probe/url", params={
+        "count": 2,
+        "url": "localhost:8000",
+        "delay": 2, 
+        "back_off": 3,
+        "ssl": True
+    })
+    assert response.json() == {"Error": "Please provide the full URL of the web app to test. i.e. https://localhost"}
