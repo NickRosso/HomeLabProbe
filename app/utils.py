eb56c89928aaee5ddb275cd65d821e25bc323633
NickRosso <nicholasrosso@comcast.net>
2026-02-20 23:13:31 -0600

    added two notes

--- app/utils.py
+++ app/utils.py
@@ -7,12 +7,13 @@ def validate_and_probe_subnet(subnet):
         network = ipaddress.ip_network(subnet, strict=True) 
     except ValueError: 
         raise HTTPException(status_code=400, detail="Invalid subnet format. Use CIDR notation like 192.168.1.0/28.")
-    
+    #TODO: this first octet check does not appear to be working based on unit test coverage
     first_octet = int(str(network.network_address).split(".")[0])
     if not (192 <= first_octet <= 223):
         raise HTTPException(status_code=400, detail="Subnet must be in the Class C range (192.0.0.0 – 223.255.255.255).")
 
     # Validate prefix length (must be /24 or smaller)
+    #TODO: this first prefix check does not appear to be working based on unit test coverage
     if network.prefixlen < 24:
         raise HTTPException(status_code=400, detail="Subnet must be /24 or smaller (e.g., /24, /25, /26, /27, /28).")
     
